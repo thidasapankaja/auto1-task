04de0786cc7582bd4797bcfb173c082195d7e68a
thidasapankaja <thidasapankaja@gmail.com>
2022-11-20 21:35:33 +0530

    [added] README

--- src/continers/Car/Car.test.tsx
+++ src/continers/Car/Car.test.tsx
@@ -2,6 +2,7 @@ import {render, screen} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "../../app/store";
+// @ts-ignore
 import {LocalStorageMock} from "@react-mock/localstorage";
 
 import Car from "./index";
