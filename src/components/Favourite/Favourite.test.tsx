fdae7a341e188801cef451d4dd3195511ceb8d6a
thidasapankaja <thidasapankaja@gmail.com>
2022-11-20 21:38:29 +0530

    [fixed] typescript issues

--- src/components/Favourite/Favourite.test.tsx
+++ src/components/Favourite/Favourite.test.tsx
@@ -2,11 +2,12 @@ import {render, screen} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "../../app/store";
+// @ts-ignore
 import {LocalStorageMock} from "@react-mock/localstorage";
 
 import Favourite from "./index";
 
-const renderComponent = (props) =>
+const renderComponent = (props: any) =>
   render(
     <BrowserRouter>
       <Provider store={store}>
@@ -22,14 +23,13 @@ test("renders a favourite car", () => {
 
   expect(screen.getByTestId("favourite-button")).toBeInTheDocument();
   expect(screen.getByText("Save")).toBeInTheDocument();
-  expect(screen.queryByText("Unsave"))?.not.toBeInTheDocument()
+  expect(screen.queryByText("Unsave"))?.not.toBeInTheDocument();
 });
 
 test("renders a not favourite car", () => {
-    renderComponent({isFavourite: true});
-  
-    expect(screen.getByTestId("favourite-button")).toBeInTheDocument();
-    expect(screen.getByText("Unsave")).toBeInTheDocument();
-    expect(screen.queryByText("Save"))?.not.toBeInTheDocument()
-  });
-  
+  renderComponent({isFavourite: true});
+
+  expect(screen.getByTestId("favourite-button")).toBeInTheDocument();
+  expect(screen.getByText("Unsave")).toBeInTheDocument();
+  expect(screen.queryByText("Save"))?.not.toBeInTheDocument();
+});
